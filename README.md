9be226abddbba520dd5573a752e96050b5703cc3
Soger666 <tegor2230@gmail.com>
2023-05-21 15:13:55 +0500

    furstcomm

---
+++ README.md
@@ -0,0 +1,1 @@
+This project was created to advertise discounts on watermelons
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Action!!! Buy 5 watermelons and get a 10% discount forever!!!";
+	return 0;
+}
